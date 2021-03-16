f3be7c5baec39430a0a9f5c3bf34c6d65933723d
Cristian David Henao Vergel <cristian.1701823811@ucaldas.edu.co>
2021-03-16 16:28:14 -0500

    Se crean los modelos de mysql y mongodb

---
+++ src/models/mascota.model.ts
@@ -0,0 +1,51 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Mascota extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  identificador: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  fechaNacimiento: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  descripcion: string;
+
+  @property({
+    type: 'string',
+    required: false,
+  })
+  imagen: string;
+
+  constructor(data?: Partial<Mascota>) {
+    super(data);
+  }
+}
+
+export interface MascotaRelations {
+  // describe navigational properties here
+}
+
+export type MascotaWithRelations = Mascota & MascotaRelations;
